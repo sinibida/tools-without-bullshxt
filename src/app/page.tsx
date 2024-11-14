285151dfbece734f6a859f62962ec0bf1821e955
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-11-15 03:15:34 +0900

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -23,8 +23,13 @@ export default function Home() {
     <div className={styles.container}>
       <nav>
         <div className={styles.titleBox}>
-          <h1>Tools Without Bullsh<span style={{color: 'var(--accent)'}}>x</span>t.</h1>
-          <p className={styles.byMe}>by <span>SPAUPA</span></p>
+          <h1>
+            Tools Without Bullsh
+            <span style={{ color: "var(--accent)" }}>x</span>t.
+          </h1>
+          <p className={styles.byMe}>
+            by <span>SPAUPA</span>
+          </p>
         </div>
       </nav>
       <main>
@@ -32,6 +37,14 @@ export default function Home() {
           {data && <ToolCardList ids={data.cardIds} />}
         </Loading>
       </main>
+      <footer>
+        <svg viewBox="0 0 40 4" width="40" height="4">
+          <path
+            d="M0,0 l4,4 l4,-4 l4,4 l4,-4 l4,4 l4,-4 l4,4 l4,-4 l4,4 l4,-4 "
+          />
+        </svg>
+        <p>SPAUPA, 2024.</p>
+      </footer>
     </div>
   );
 }
