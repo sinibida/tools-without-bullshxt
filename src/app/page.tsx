3ffc530aa149ef15f0c7ff975d6f0a12bf1b54d4
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-11-15 03:02:27 +0900

    feat: css!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,37 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import ToolCardList from "@/widgets/ToolCardList";
+import { useQuery } from "@tanstack/react-query";
+import { getAllTools } from "@/entities/dto/ToolDto";
+import Loading from "@/components/layouts/Loading";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const query = useQuery({
+    queryKey: ["Home"],
+    queryFn: async () => {
+      return {
+        cardIds: (await getAllTools()).map((x) => x.id),
+      };
+    },
+  });
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const { data } = query;
+
+  return (
+    <div className={styles.container}>
+      <nav>
+        <div className={styles.titleBox}>
+          <h1>Tools Without Bullsh<span style={{color: 'var(--accent)'}}>x</span>t.</h1>
+          <p className={styles.byMe}>by <span>SPAUPA</span></p>
         </div>
+      </nav>
+      <main>
+        <Loading query={query}>
+          {data && <ToolCardList ids={data.cardIds} />}
+        </Loading>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
