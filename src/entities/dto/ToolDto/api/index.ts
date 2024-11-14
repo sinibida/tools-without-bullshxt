2e14392fbb9e59828a95b6f32d6c0557d22cf3be
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-11-15 03:37:41 +0900

    minimal sort

--- src/entities/dto/ToolDto/api/index.ts
+++ src/entities/dto/ToolDto/api/index.ts
@@ -1,19 +1,22 @@
 import { http } from "@/shared/api/http";
 import { ToolDto } from "../model";
+import { sortTools } from "../lib/sort";
 
 export async function getAllTools() {
-  const response = await http.get('tools');
+  const response = await http.get("tools");
 
-  return response.data as ToolDto[];
+  const data = response.data as ToolDto[];
+
+  return sortTools(data);
 }
 
 export async function getTool(id: number) {
   const tools = await getAllTools();
 
-  const ret = tools.find(x => x.id === id);
+  const ret = tools.find((x) => x.id === id);
 
   if (ret === undefined) {
-    throw Error("Tool not found.")
+    throw Error("Tool not found.");
   }
 
   return ret;
---
+++ src/entities/dto/ToolDto/lib/sort.ts
@@ -0,0 +1,29 @@
+import { ToolDto } from "../model";
+
+function compareStringArray(a: string[], b: string[]) {
+  let at = 0;
+  while (at < a.length || at < b.length) {
+    if (at >= a.length) return -1;
+    if (at >= b.length) return 1;
+
+    const aa = a[at];
+    const bb = b[at];
+    if (aa > bb) return 1;
+    if (aa < bb) return -1;
+  }
+
+  return 0;
+}
+
+export function sortTools(data: ToolDto[]) {
+  const t = data.map((x) => ({
+    ref: x,
+    sortedTags: x.tags.sort(),
+  }));
+
+  const sorted = t.sort((a, b) =>
+    compareStringArray(a.sortedTags, b.sortedTags)
+  );
+
+  return sorted.map((x) => ({...x.ref, tags: x.sortedTags}));
+}
